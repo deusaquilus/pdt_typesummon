ce73b87e0b1444f1e9c4fe77af2eaa33217e3ff2
deusaquilus <deusaquilus@gmail.com>
2020-05-08 10:38:20 -0400

    Move impl code to test

--- src/main/scala/bug/Macro.scala
+++ src/main/scala/bug/Macro.scala
@@ -15,21 +15,14 @@ trait Context {
   def serve[T](decoder: Decoder[RowType, T]): T
 }
 
-class MyContext extends Context {
-  type RowType = Row1
-  implicit def stringDecoder: Decoder[Row1, String] =
-    new Decoder[Row1, String] {
-      def decode(row: Row1): String = stringDecoder.decode(row)
-    }
-  val internalRow = Row1("hello")
-  def serve[T](decoder: Decoder[Row1, T]): T = decoder.decode(internalRow)
-}
-
 object Macro {
 
   inline def serveDecoder[T](context: Context): T = ${ serveDecoderImpl[T]('context) }
   def serveDecoderImpl[T: Type](context: Expr[Context])(using qctx: QuoteContext): Expr[T] = {
     import qctx.tasty._
+
+    val dect = '[$context.RowType]
+
     val tpe = '[Decoder[$context.RowType, T]]
     val decoderExpr = 
       Expr.summon(using tpe) match {
--- src/main/scala/bug/Test.scala
+++ src/main/scala/bug/Test.scala
@@ -2,6 +2,16 @@ package bug
 
 import Macro._
 
+class MyContext extends Context {
+  type RowType = Row1
+  implicit def stringDecoder: Decoder[Row1, String] =
+    new Decoder[Row1, String] {
+      def decode(row: Row1): String = stringDecoder.decode(row)
+    }
+  val internalRow = Row1("hello")
+  def serve[T](decoder: Decoder[Row1, T]): T = decoder.decode(internalRow)
+}
+
 @main def testMacro() = {
   val ctx = new MyContext()
   import ctx._
