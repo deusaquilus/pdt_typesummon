f2c14bd84f85c1a3ee1eec39e71cc7f184e894e9
deusaquilus <deusaquilus@gmail.com>
2020-05-08 12:24:09 -0400

    Extraction of RowType into a parameter does not work

--- src/main/scala/bug/Macro.scala
+++ src/main/scala/bug/Macro.scala
@@ -15,11 +15,11 @@ trait Context {
 
 object Macro {
 
-  inline def serveDecoder[T](context: Context): T = ${ serveDecoderImpl[T]('context) }
-  def serveDecoderImpl[T: Type](context: Expr[Context])(using qctx: QuoteContext): Expr[T] = {
+  inline def serveDecoder[T](context: Context): T = ${ serveDecoderImpl[T, context.RowType, Context]('context) }
+  def serveDecoderImpl[T: Type, U: Type, C <: Context { type RowType = U }: Type](context: Expr[Context])(using qctx: QuoteContext): Expr[T] = {
     import qctx.tasty._
 
-    val tpe = '[Decoder[$context.RowType, T]]
+    val tpe = '[Decoder[U, T]]
     val decoderExpr = 
       Expr.summon(using tpe) match {
         case Some(decoder) => decoder
